b3ff446f018f34470c4350a60b961efeb2910a11
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-01-12 22:14:04 +0100

    Getting there

--- index.js
+++ index.js
@@ -4,15 +4,28 @@
     - help
     - help if no argv2
 */
+var argv = require('minimist')(process.argv.slice(2), {
+    default : {
+        newline : true,
+    }
+})
 
 function ref(obj, str) {
-    str = str.split(".");
-    for (var i = 0; i < str.length; i++) {
-        obj = obj[str[i]];
-    }
-    return obj;
+    return str.split('.').reduce(function(last, curr) {
+        return last[curr]
+    }, obj) 
 }
-//console.log(process.argv[3])
+
+function halp() {
+    console.log('USAGE')
+    console.log('    jsonpick [OPTIONS] <file.json> key.path.to.value')
+    console.log('OPTIONS')
+    console.log('    newline - Print a newline char (default true)')
+}
+
+if (argv.h || argv.help) { halp(); process.exit(0) }
+if (process.argv.length < 3) { halp(); process.exit(1) }
 var data = require(process.argv[2])
-if (!process.argv[3]) console.log(data)
-else console.log(ref(data, process.argv[3]))
+if (!process.argv[3]) { console.log(data); process.exit(0) }
+if (typeof argv.newline == 'boolean') console.log(ref(data, process.argv[3]))
+else process.stdout.write(ref(data, process.argv[3]))
